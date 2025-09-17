45cbdbad7dd0e2d98751210ed36bb467f1a7999b
Hiroshi-Hashimoto <hiroshi.hashimoto@58web3.io>
2025-09-17 13:21:16 +0900

    add: workflows/deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://hiroshi-hashimoto-58.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'hiroshi-hashimoto-58', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
